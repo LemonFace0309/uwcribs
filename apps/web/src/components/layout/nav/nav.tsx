e378eb604874c1c3dc2963ef197a7d692f17ebe6
Charles Liu <charles.liu0309@gmail.com>
2022-03-07 04:42:02 -0500

    added small animation

--- apps/web/src/components/layout/nav/nav.tsx
+++ apps/web/src/components/layout/nav/nav.tsx
@@ -4,7 +4,7 @@ import { Button, Logo } from '@root/ui/components';
 
 export const Nav: VFC = () => (
   <nav className="flex justify-between p-4">
-    <Logo className="cursor-pointer" onClick={() => null} />
+    <Logo className="cursor-pointer transition hover:-rotate-3 hover:scale-105" onClick={() => null} />
     <Button color="salmon" variant="rounded">Login</Button>
   </nav>
 );
