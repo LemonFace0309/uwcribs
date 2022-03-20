646ca128730aaa44c28aa57a50027714e1c2a662
Charles Liu <charles.liu0309@gmail.com>
2022-03-20 04:23:06 -0400

    Feat/auth (#1)
    * added next-auth
    
    * added facebook login
    
    * modified yarn lock

---
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth from 'next-auth';
+import FacebookProvider from 'next-auth/providers/facebook';
+
+export default NextAuth({
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    redirect({ url, baseUrl }) {
+      if (url.startsWith(baseUrl)) return url;
+      // Allows relative callback URLs
+      else if (url.startsWith('/')) return new URL(url, baseUrl).toString();
+      return baseUrl;
+    },
+  },
+});
