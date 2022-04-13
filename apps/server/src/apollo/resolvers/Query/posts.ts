861439d5bc58c58927e0ad30f1f55e060e1cb375
Charles Liu <charles.liu0309@gmail.com>
2022-04-12 23:05:04 -0400

    works with es search now

--- apps/server/src/apollo/resolvers/Query/posts.ts
+++ apps/server/src/apollo/resolvers/Query/posts.ts
@@ -1,6 +1,23 @@
+import type { QueryPostsArgs } from "@src/__generated__/graphql";
 import type { Context } from "@src/server/context";
 
-export const posts = async (_root: unknown, _args: never, ctx: Context) => {
+export const posts = async (
+  _root: unknown,
+  args: QueryPostsArgs,
+  ctx: Context
+) => {
+  if (args.options && args.options.beds) {
+    const res = await ctx.elastic.search({
+      index: "posts_v1",
+      query: {
+        match: {
+          availableBeds: args.options.beds,
+        },
+      },
+    });
+    return res.hits.hits.map((obj) => obj._source);
+  }
+
   return await ctx.prisma.post.findMany({
     where: {
       flagged: false,
