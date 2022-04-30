55b20256346a9c7c76288039c75cb36692867d6d
Charles Liu <charles.liu0309@gmail.com>
2022-04-30 04:34:19 -0400

    completed landing page

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -5,9 +5,19 @@ import styles from "@src/styles/pages/landing.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.root}>
-      {/* <Button href="/search">Search</Button> */}
-    </div>
+    <>
+      <div className={styles.root} />
+      <div className={styles.textContainer}>
+        <h1 className="text-6xl text-white">Find Your Dream Dorm in Seconds</h1>
+        <Button
+          className={styles.btn}
+          href="/search"
+          variant="rounded"
+          size="lg">
+          <span className="text-3xl">Start Now</span>
+        </Button>
+      </div>
+    </>
   );
 };
 
