76aa00eb42cad0db965dd892e166171bed95b99e
Chamod Gamage <chamod.gamage@beondeck.com>
2022-05-01 20:53:31 -0400

    chore(parser): mongoose collection names forced

--- apps/parser/models/group.js
+++ apps/parser/models/group.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-const GroupSchema = new mongoose.Schema({
-  id: { type: String, required: true, index: { unique: true } },
-  name: { type: String },
-});
+const GroupSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true, index: { unique: true } },
+    name: { type: String },
+  },
+  { collection: "Group" }
+);
 
 const Group = mongoose.model("Group", GroupSchema);
 
--- apps/parser/models/post.js
+++ apps/parser/models/post.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 
-const PostSchema = new mongoose.Schema({
-  id: { type: String, required: true, index: { unique: true } },
-  text: { type: String, required: true, default: "" },
-  groupId: { type: mongoose.Types.ObjectId, required: true },
-  author: { type: String, default: "" },
-  availableBeds: { type: Number },
-  totalBeds: { type: Number },
-  baths: { type: Number },
-  flagged: { type: Boolean, default: false },
-  confirmed: { type: Boolean, default: false },
-  amenities: { type: [String], default: [] },
-  description: { type: String, default: "" },
-  title: { type: String, default: "" },
-  imageUrl: { type: String, default: "" },
-  ppp: { type: Number, default: 0 },
-  fbLink: { type: String, default: "" },
-  genderRestriction: {
-    type: String,
-    default: "coed",
-    enum: ["coed", "male", "female"],
+const PostSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true, index: { unique: true } },
+    text: { type: String, required: true, default: "" },
+    groupId: { type: mongoose.Types.ObjectId, required: true },
+    author: { type: String, default: "" },
+    availableBeds: { type: Number },
+    totalBeds: { type: Number },
+    baths: { type: Number },
+    flagged: { type: Boolean, default: false },
+    confirmed: { type: Boolean, default: false },
+    amenities: { type: [String], default: [] },
+    description: { type: String, default: "" },
+    title: { type: String, default: "" },
+    imageUrl: { type: String, default: "" },
+    ppp: { type: Number, default: null },
+    fbLink: { type: String, default: "" },
+    genderRestriction: {
+      type: String,
+      default: "coed",
+      enum: ["coed", "male", "female"],
+    },
+    type: { type: String, enum: ["entireRentalUnit", "sharedRentalUnit"] },
   },
-  type: { type: String, enum: ["entireRentalUnit", "sharedRentalUnit"] },
-});
+  { collection: "Post" }
+);
 
 const Post = mongoose.model("Post", PostSchema);
 
