7ea5041a7a47c63c333f89ca6aef7cb2b4fab9d4
Chamod Gamage <chamodgamage26@gmail.com>
2022-05-13 13:19:39 -0400

    feat(ui): add modal component

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,6 +2,7 @@ import React from "react";
 export * from "./button";
 export * from "./card";
 export * from "./logo";
+export * from "./modal";
 export * from "./radio";
 export * from "./select";
 export * from "./seperator";
