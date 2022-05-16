a21e7b80788728cd5c4308a37c879a2f2abfbfc2
Chamod Gamage <chamodgamage26@gmail.com>
2022-05-16 11:07:17 -0400

    fix(web): temporarily hide auth button

--- apps/web/src/components/layout/nav/nav.tsx
+++ apps/web/src/components/layout/nav/nav.tsx
@@ -5,6 +5,8 @@ import { Logo } from "@root/ui/components";
 
 import { AuthButton } from "./auth-button";
 
+const isAuthEnabled = false;
+
 export const Nav: FC = () => {
   return (
     <nav className="flex justify-between p-4 ">
@@ -13,7 +15,7 @@ export const Nav: FC = () => {
           <Logo className="cursor-pointer transition hover:-rotate-3 hover:scale-105" />
         </a>
       </Link>
-      <AuthButton />
+      {isAuthEnabled && <AuthButton />}
     </nav>
   );
 };
