e0f431561a5ab88b7117a8342e058821862c180b
Chamod Gamage <chamod.gamage@beondeck.com>
2022-05-17 22:34:04 -0400

    feat(web): redirect to search page via next config (#17)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,13 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/search",
+        permanent: false,
+      },
+    ];
+  },
 });
